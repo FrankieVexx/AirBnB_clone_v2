563c92a20bf7977b08cec79511600cb287dc3442
Francis Onyach <franblog7@gmail.com>
2023-04-06 03:36:48 -0700

    packing webstatic

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Create an archive of the webstatic directory from AIrBnB_clone_v2
+"""
+
+
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """ A function to create an archive """
+	
+	current_time = datetime.now()
+	time_string = current_time.strftime("%Y%m%d%H%M%S")
+
+	try:
+            local("mkdir -p versions")
+            local("tar -cvzf versions/web_static_{}.tgz web_static".format(time_string))
+            return ("versions/web_static_{}".format(time_string))
+
+	except Exception as error:
+            return None
+
