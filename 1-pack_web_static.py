d9734b474a070a119accb12d651064994f2179a0
Francis Onyach <franblog7@gmail.com>
2023-04-06 05:40:54 -0700

    correctiong task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,24 +1,23 @@
-#!/usr/bin/python3
 """a fabric script to create an archive file"""
 
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
     """ A function that compresses a file """
-    
+
     current_time = datetime.now().strftime("%Y%m%d%H%M%S")
     file_path = "versions/web_static_{}.tgz".format(current_time)
 
-	""" Create a directory named versions
-	create an arhcive of webstatic
-	return the path of the archive file
-	"""   
+        """ Create a directory named versions
+        create an arhcive of webstatic
+        return the path of the archive file
+	"""
     try:
         local("mkdir -p versions")
-        local("tar -cvzf {} web_static/".format(file_path))        
+	local("tar -cvzf {} web_static/".format(file_path))
         return "{}".format(file_path)
 
     except Exception as error:
         return None
-        
