ad9509a0c7b2762903a9f03f2e6abe88e861aca9
Francis Onyach <franblog7@gmail.com>
2023-04-06 15:08:27 -0700

    corrections to deploy

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,17 +12,12 @@ def do_pack():
     try:
         """create a directory called versions"""
         local("mkdir -p versions")
-<<<<<<< HEAD
          
         """create an archive for webstatic"""
         local("tar -cvzf {} web_static/".format(file_path))
         
-=======
-     
-        """create an archive for webstatic"""
         local("tar -cvzf {} web_static/".format(file_path))
      
->>>>>>> b23b4af07966bdf44444fdfe0a72fa24fdb1ec8a
         """return the path to the archive file created"""
         return "{}".format(file_path)
 
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,57 @@
 #!/usr/bin/python3
-"""A fabric script to transfer decompressed webstatic
-files to remote servers"""
+"""
+Fabric script to deploy web static content
+"""
 
-from fabric.api import run, env, put
-import os.path
+from fabric.api import env, put, run
+from os.path import exists
+import os
 
 env.hosts = ['52.91.118.25', '52.91.125.236']
-env.key_source = '~/.ssh/school'
 env.user = 'ubuntu'
+env.key_filen = '~/.ssh/school'
 
 
 def do_deploy(archive_path):
-    """ Deploy code to different webservers and decompress"""
-
-    if not os.path.isfile(archive_path):
+    """
+    Deploys an archive to the web servers
+    """
+    """ Check for archive_path """
+    if not exists(archive_path):
         return False
-    compressed_file = archive_path.split("/")[-1]
-    no_extension = compressed_file.split(".")[0]
+
+    """ file names with and without extension """
+    arch_name = os.path.basename(archive_path)
+    arch_name_minus = os.path.splitext(arch_name)[0]
 
     try:
-        remote_path = "/data/web_static/releases/{}/".format(no_extension)
-        sym_link = "/data/web_static/current"
+        """ Save archive to tmp on the web servers """
         put(archive_path, "/tmp/")
-        run("sudo mkdir - p {}".format(remote_path))  #make directory
-        run("sudo tar -xvzf /tmp/{} -C {}".format(compressed_file, remote_path))
 
-    """remove compressed file"""
-        run("sudo rm /tmp/{}".format(compressed_file)) 
-    """Create symbolic link"""
-        run("sudo ln -sf {} {}".format(remote_path, sym_link))
+        """ Create directory for the deployed files"""
+        run("sudo mkdir -p /data/web_static/releases/{}/"
+            .format(archive_name_no_ext))
+
+        """ Decompress the archive into the we_static folder """
+        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(archive_name, archive_name_no_ext))
+
+        """ Delete the archive from the server """
+        run("sudo rm /tmp/{}".format(archive_name))
+
+        """ Move the files to a new folder and delete the old symbolic link """
+        run("sudo mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/"
+            .format(archive_name_no_ext, archive_name_no_ext))
+        run("sudo rm -rf /data/web_static/releases/{}/web_static"
+            .format(archive_name_no_ext))
+
+        """ Delete the old symbolic link and create a new one """
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{}/ \
+                /data/web_static/current".format(archive_name_no_ext))
+
         return True
+
     except Exception as error:
         return False
