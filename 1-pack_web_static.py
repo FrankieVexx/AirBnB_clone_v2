b23b4af07966bdf44444fdfe0a72fa24fdb1ec8a
Francis Onyach <franblog7@gmail.com>
2023-04-06 06:55:34 -0700

    excecute pack webstatic

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,24 @@
-"""a fabric script to create an archive file"""
+#!/usr/bin/python3
+"""A script to create an archive file webstatic"""
 from fabric.api import local
 from datetime import datetime
 
 def do_pack():
-    """ a method to compress a file and return it's path """
+    """ Function to compress a file """
     
-    """saving the current timestamp and creatinf filename"""
-    time_now = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_path = "versions/web_static_{}.tgz".format(time_now)
+    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_path = "versions/web_static_{}.tgz".format(current_time)
    
     try:
         """create a directory called versions"""
         local("mkdir -p versions")
-         
-        """create an archive file"""
+     
+        """create an archive for webstatic"""
         local("tar -cvzf {} web_static/".format(file_path))
-        
+     
         """return the path to the archive file created"""
         return "{}".format(file_path)
 
-        """return none if an error occurs"""
+        """return exception errors"""
     except Exception as e:
         return None
-        
