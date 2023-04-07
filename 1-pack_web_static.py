3397a59992860373eecf364ee9669c25417c539d
Francis Onyach <franblog7@gmail.com>
2023-04-07 04:55:51 -0700

    correction indentation

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,21 +3,22 @@
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
     """ Function to compress a file """
-    
+
     current_time = datetime.now().strftime("%Y%m%d%H%M%S")
     file_path = "versions/web_static_{}.tgz".format(current_time)
-   
+
     try:
         """create a directory called versions"""
         local("mkdir -p versions")
-         
+
         """create an archive for webstatic"""
         local("tar -cvzf {} web_static/".format(file_path))
-        
+
         local("tar -cvzf {} web_static/".format(file_path))
-     
+
         """return the path to the archive file created"""
         return "{}".format(file_path)
 
