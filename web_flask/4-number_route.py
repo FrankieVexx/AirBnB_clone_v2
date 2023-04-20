e72688fe6d6bd13078de95ed3ec43435cbcbbffd
Francis Onyach <franblog7@gmail.com>
2023-04-20 06:37:47 -0700

    Display an integer

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" a flask script that print line of text """
+
+from flask import Flask
+app = Flask(__name__)
+@app.route("/", strict_slashes=False)
+def home():
+    """function that prnts 'Hello hbnb' """
+    return "Hello HBNB!"
+@app.route("/hbnb", strict_slashes=False)
+def index():
+    """ a function that returns hbnb """
+    return "HBNB"
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    """ display "c is fun """
+    return f'C {text.replace("_", " ")}'
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py_test(text='is cool'):
+    """display "Python " + the text variable if provided"""
+    return f'Python {text.replace("_", " ")}'
+@app.route('/number/<n>', strict_slashes=False)
+def numb(n):
+    """ Display an integer"""
+    n = int(n)
+    return f'{n} is a number'
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
