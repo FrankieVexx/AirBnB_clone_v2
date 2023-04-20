411e93a2cdf25e2cb7f044d2ed3b00325a0c8340
Francis Onyach <franblog7@gmail.com>
2023-04-20 04:56:49 -0700

    Setting up flask and running first file

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" a flask script that print line of text """
+
+from flask import Flask
+app = Flask(__name__)
+@app.route("/", strict_slashes=False)
+def home():
+    """function that prnts 'Hello hbnb' """
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ web_flask/README.md
@@ -0,0 +1,1 @@
+Setting up and using flask in a linux environment
