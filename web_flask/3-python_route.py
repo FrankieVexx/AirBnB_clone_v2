aa2bf9fc90334b2f04c07a6585508f6d6fc40076
Francis Onyach <franblog7@gmail.com>
2023-04-21 01:02:18 -0700

    correct task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,7 +27,7 @@ def c_is_fun(text):
 @app.route('/python/<text>', strict_slashes=False)
 def py_test(text='is cool'):
     """display "Python " + the text variable if provided"""
-    return f'Python {text.replace("_", " ")}'
+    return 'Python' + text.replace("_", " ")
 
 
 if __name__ == "__main__":
